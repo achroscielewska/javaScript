7f97d8a9c39f5481005fe2b4d921897e79a89433
chroagni <Agnieszka.Chroscielewska@dat.eu>
2021-03-21 19:47:06 +0100

    quick-sort

---
+++ sortingAlgorithms/quick-sort.js
@@ -0,0 +1,24 @@
+function quickSort(arr) {
+    if(arr.length===0) return [];
+ 
+    let left = [];
+    let right = [];
+    const pivot = arr[0];
+
+    for (let i=1; i<arr.length; i++) {
+        if(arr[i] < pivot) {
+            left.push(arr[i]);
+        } else {
+            right.push(arr[i]);
+        }
+    }
+    
+    return quickSort(left).concat(pivot, quickSort(right));
+};
+
+let a = [];
+for( let i=0; i<10; i++) {
+    a[i] = Math.floor(Math.random()*100+1);
+}
+
+console.log(quickSort(a));
